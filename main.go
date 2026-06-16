0aa74f15f41daf794d0fffa194bc00f22e3c065f
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2026-06-16 19:42:14 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"time"
+
+	"charm.land/bubbles/v2/progress"
+	tea "charm.land/bubbletea/v2"
+)
+
+type model struct {
+	progress progress.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return tickCmd()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		}
+	case tickMsg:
+		if m.progress.Percent() == 1.0 {
+			return m, tea.Quit
+		}
+		cmd := m.progress.IncrPercent(1.0 / 60.0)
+		return m, tea.Batch(cmd, tickCmd())
+	case progress.FrameMsg:
+		var cmd tea.Cmd
+		m.progress, cmd = m.progress.Update(msg)
+		return m, cmd
+
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	return tea.NewView(m.progress.View())
+}
+
+func main() {
+	m := model{
+		progress: progress.New(progress.WithDefaultBlend()),
+	}
+
+	p := tea.NewProgram(m)
+	p.Run()
+}
+
+type tickMsg time.Time
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second*1, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
